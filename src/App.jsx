8b415fc0d8054c08d73d4b8865fe61ce6a843a49
Luis Acevedo <luisemilio.acevedo@globant.com>
2024-03-02 17:21:41 -0500

    funcionabilidad casi-completa

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
+const movies = [
+  { name: "Forrest Gump", emoji: "🏃🍫🍤" },
+  { name: "The Matrix", emoji: "🕶️💊👽" },
+  { name: "Titanic", emoji: "🚢❄️💔" },
+  { name: "Jurassic Park", emoji: "🦖🌴🚙" },
+  { name: "The Lion King", emoji: "🦁👑🌅" },
+  { name: "Star Wars", emoji: "⚔️🚀🌌" },
+  { name: "The Avengers", emoji: "🦸‍♂️🦸‍♀️💥" },
+  { name: "Harry Potter", emoji: "⚡🧙‍♂️🔮" },
+  { name: "The Terminator", emoji: "🤖🔫🕶️" },
+  { name: "Indiana Jones", emoji: "🤠🔍💎" },
+  { name: "Back to the Future", emoji: "⏰🚗💥" },
+  { name: "The Shawshank Redemption", emoji: "🔒🔑💰" },
+  { name: "The Godfather", emoji: "🍕🤵🔫" },
+  { name: "The Dark Knight", emoji: "🦇♞👨‍🦯" },
+  { name: "Pulp Fiction", emoji: "🍔🔫🕶️" },
+  { name: "Schindler's List", emoji: "📜🚂🔴" },
+  { name: "The Lord of the Rings: The Return of the King", emoji: "🧝‍♂️🧙‍♂️🗡️" },
+  { name: "The Silence of the Lambs", emoji: "🔇🐑🍖" },
+  { name: "Fight Club", emoji: "👊💼🚽" },
+  { name: "Inception", emoji: "🌀👩‍🚀🎩" },
+  { name: "The Shawshank Redemption", emoji: "🔒🔑💰" },
+  ];
+
+  
 function App() {
+
+  function validar() {
+    if(movie.name.toUpperCase() == inputValue.toUpperCase()) {
+      setCount(count + 1);
+      setInputValue("");
+    }else{
+      setLive(live - 1)
+    }
+  }
+  
   const [count, setCount] = useState(0)
+  const [live, setLive] = useState(3)
+  const [movie, setMovie] = useState("")
+  const [inputValue, setInputValue] = useState("")
+
+  useEffect(() => {
+    setMovie(movies[Math.floor(Math.random() * movies.length)]);
+  },[count])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <h3 className='live'>live: {live}</h3>
+      <h3 className='point'>Point: {count}</h3>
+      <h1>Guess Movie</h1>
+      <h2>{movie.emoji}</h2>
+      <div className='form-response'>
+        <input type="text" id="response" placeholder="Insert movie´s name" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+        <button onClick={validar}>Send</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
